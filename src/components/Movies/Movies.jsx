d6c600fb9e236a4ad35bbe501a97570c045d3cf9
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-07-12 21:37:04 -0400

    Rendering Movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,12 +3,37 @@ import { Box, CircularProgress, useMediaQuery, Typography } from "@mui/material"
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../Services/TMDB';
+import { MovieList } from '..';
+
 
 const Movies = () => {
-    const { data } = useGetMoviesQuery();
+    const { data, isFetching, error } = useGetMoviesQuery();
     console.log(data);
+    if (isFetching) {
+        return (
+            <Box display='flex' justifyContent='center'>
+                <CircularProgress size='4rem' />
+            </Box>
+        );
+    }
+
+    if (!data.results.length) {
+        return (
+            <Box display='flex' alignItems='center' mt='20px'>
+                <Typography variant='h4'>
+                    No movies with that name.
+                    <br />
+                    Please search for something else.
+                </Typography>
+            </Box>
+        );
+    }
+    if (error) return 'An error has occured';
+
     return (
-        <div>Movies</div>
+        <div>
+            <MovieList movies={data} />
+        </div>
     );
 };
 
