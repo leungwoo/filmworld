a2c2b9215aaea422a400621bd7d60e49e40d284d
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-07-13 16:17:09 -0400

    Finishing Movie View

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,6 +4,27 @@ export default makeStyles((theme) => ({
     movie: {
         padding: '10px',
     },
+
+    links: {
+        alignItems: 'center',
+        fontWeight: 'bold',
+        textDecoration: 'none',
+        [theme.breakpoints.up('xs')]: { display: 'flex', flexDirection: 'column' },
+        '&:hover': {
+            cursor: 'pointer',
+            TextDecoration: 'none'
+        },
+    },
+    image: {
+        borderRadius: '20px',
+        height: '300px',
+        marginBottom: '10px',
+        '&:hover': {
+            cursor: 'pointer',
+            textDecoration: 'none',
+            transform: 'scale(1.05)',
+        },
+    },
     title: {
         color: theme.palette.text.primary,
         textOverflow: 'ellipsis',
@@ -12,6 +33,9 @@ export default makeStyles((theme) => ({
         marginTop: '10px',
         marginBottom: 0,
         textAlign: 'Center',
-        whiteSpace: 'nowrap'
-    }
+        whiteSpace: 'nowrap',
+        padding: '8px',
+
+    },
+
 }));
